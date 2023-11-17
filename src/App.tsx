6d1017b694b8253a229218f09773d2b00c6276dc
usuariosilveira <moniquesilveiraa28@gmail.com>
2023-11-17 08:49:28 -0300

    atualizando código

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Cadastro from './pages/cadastro/Cadastro';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
+import FormularioPostagem from './components/postagens/formularioPostagens/FormularioPostagem';
+import Perfil from './pages/Perfil';
+<Route path="/perfil" element={<Perfil />} />
+
+function App() {
+    return (
+        <AuthProvider>
+            <BrowserRouter>
+                <Navbar />
+                <div className='min-h-[80vh]'>
+                    <Routes>
+                        <Route path="/" element={<Login />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/cadastro" element={<Cadastro />} />
+                        <Route path="/temas" element={<ListaTemas />} />
+                        <Route path="/cadastroTema" element={<FormularioTema />} />
+                        <Route path="/editarTema/:id" element={<FormularioTema />} />
+                        <Route path="/deletarTema/:id" element={<DeletarTema />} />
+                        <Route path="/postagens" element={<ListaPostagens />} />
+                        <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+                        <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </BrowserRouter>
+        </ AuthProvider>
+    );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,38 @@
+import { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { AuthContext } from '../../contexts/AuthContext'
+
+function Navbar() {
+
+    const navigate = useNavigate()
+    const { handleLogout } = useContext(AuthContext)
+
+    function logout() {
+        handleLogout()
+        alert('Usuário deslogado com sucesso')
+        navigate('/login')
+    }
+
+    return (
+        <>
+            <div className='w-full bg-indigo-900 text-white
+                flex justify-center py-4'>
+
+                <div className="container flex justify-between text-lg">
+                    <Link to='/home' className='text-2xl font-bold'>Blog Pessoal</Link>
+
+                    <div className='flex gap-4'>
+                        <Link to='/postagens' className='hover:underline'>Postagens</Link>
+                        <Link to='/temas' className='hover:underline'>Temas</Link>
+                        <Link to='/cadastroTema' className='hover:underline'>Cadastrar Tema</Link>
+                        Perfil
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
